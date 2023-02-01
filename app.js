fea00e0c72878901c57581efaa7f0a67f9e3eaef
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-01 01:40:20 +0000

    fdd

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
+var http = require("http");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+
+
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
 
 
 const html = `
